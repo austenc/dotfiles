636ef0ef93db76ac13057ff1130c5d4ebb70c7db
Austen Cameron <austen.cameron@gmail.com>
2023-12-04 10:34:32 -0700

    wip

---
+++ nvim/init.lua
@@ -0,0 +1,14 @@
+
+-- if vim.g.vscode then
+    -- VSCode extension
+    vim.keymap.set('n', '<S-Up>', 'v<Up>', { remap = false, silent = true })
+    vim.keymap.set('x', '<S-Up>', '<Up>', { remap = false, silent = true })
+    vim.keymap.set('n', '<S-Down>', 'v<Down>', { remap = false, silent = true })
+    vim.keymap.set('x', '<S-Down>', '<Down>', { remap = false, silent = true })
+    vim.keymap.set('n', '<S-Left>', 'v<Left>', { remap = false, silent = true })
+    vim.keymap.set('x', '<S-Left>', '<Left>', { remap = false, silent = true })
+    vim.keymap.set('n', '<S-Right>', 'v<Right>', { remap = false, silent = true })
+    vim.keymap.set('x', '<S-Right>', '<Right>', { remap = false, silent = true })
+-- end
+
+-- Normal Neovim config
