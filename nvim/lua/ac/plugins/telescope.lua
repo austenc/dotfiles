9d9670a359ea3bad13c794fb18a3ae6b212c1190
Austen Cameron <austen.cameron@gmail.com>
2024-10-14 12:07:55 -0600

    wip

--- nvim/lua/ac/plugins/telescope.lua
+++ nvim/lua/ac/plugins/telescope.lua
@@ -2,13 +2,14 @@ return {
     'nvim-telescope/telescope.nvim', branch = '0.1.x',
     dependencies = { 
         'nvim-lua/plenary.nvim',
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'makhttps://github.com/jesseleite/dotfiles/blob/master/nvim/after/ftplugin/help.luae' },
     },
     keys = {
         {'<Leader>f', function() require('telescope.builtin').git_files() end},
         {'<Leader>F', function() require('telescope.builtin').find_files() end},
         {'<Leader>h', function() require('telescope.builtin').oldfiles({ cwd_only = true }) end},
-        {'<Leader>H', function() require('telescope.builtin').help_tags() end},
+        {'<Leader>H', function() require('telescope.builtin').oldfiles({ cwd_only = true }) end},
+        {'<Leader>?', function() require('telescope.builtin').help_tags() end},
         {'<Leader>k', function() require('telescope.builtin').keymaps() end},
     },
     cmd = { 'Telescope' },
