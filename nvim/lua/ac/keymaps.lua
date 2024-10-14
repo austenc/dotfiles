9b7033411f300e1a9ba854de5f4ab62d659c0089
Austen Cameron <austen.cameron@gmail.com>
2024-10-14 10:22:16 -0600

    wip

--- nvim/lua/ac/keymaps.lua
+++ nvim/lua/ac/keymaps.lua
@@ -1,3 +1,17 @@
 -- Use spacebar as Leader key
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
+-- Window management
+-- Vertical split
+vim.keymap.set('n', '<Leader>v', vim.cmd.vsplit, { silent = true })
+
+-- Cycle through windows
+vim.keymap.set('n', '<Tab>', '<C-w>w')
+vim.keymap.set('n', '<S-Tab>', '<C-w>W')
+
+-- Make window only window
+vim.keymap.set('n', '<Leader>O', '<C-w>o')
+
+-- Swap between previously active file
+vim.keymap.set('n', '<Leader>p', '<c-^>')
--- nvim/lua/ac/plugins/telescope.lua
+++ nvim/lua/ac/plugins/telescope.lua
@@ -1,10 +1,39 @@
 return {
     'nvim-telescope/telescope.nvim', branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = { 
+        'nvim-lua/plenary.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
     keys = {
-    	{'<Leader>f', function() require('telescope.builtin').git_files() end},
-    	{'<Leader>F', function() require('telescope.builtin').find_files() end},
-    	{'<Leader>h', function() require('telescope.builtin').oldfiles() end},
-    	{'<Leader>H', function() require('telescope.builtin').help_tags() end},
-    }
- }
+        {'<Leader>f', function() require('telescope.builtin').git_files() end},
+        {'<Leader>F', function() require('telescope.builtin').find_files() end},
+        {'<Leader>h', function() require('telescope.builtin').oldfiles() end},
+        {'<Leader>H', function() require('telescope.builtin').help_tags() end},
+    },
+    config = function ()
+        local telescope = require('telescope')
+        telescope.setup({
+            defaults = {
+                sorting_strategy = "ascending",
+                layout_config = {
+                    prompt_position = "top",
+                },
+                file_ignore_patterns = {
+                    'node_modules',
+                    '.DS_Store',
+                    '.git/',
+                    'resources/dist',
+                    'storage/framework',
+                },
+            },
+            pickers = {
+                find_files = {
+                    prompt_title = 'All Files',
+                    no_ignore = true,
+                    hidden = true,
+                },
+            },
+        })
+        telescope.load_extension('fzf')
+    end,
+}
