88097d7b1006834a8256a7bf678824a42532d1d2
Austen Cameron <austen.cameron@gmail.com>
2024-10-15 08:02:51 -0600

    wip

--- nvim/lua/ac/keymaps.lua
+++ nvim/lua/ac/keymaps.lua
@@ -9,6 +9,8 @@ vim.keymap.set('n', '<Leader>v', vim.cmd.vsplit, { silent = true })
 -- Cycle through windows
 vim.keymap.set('n', '<Tab>', '<C-w>w')
 vim.keymap.set('n', '<S-Tab>', '<C-w>W')
+
+-- Remap ctrl+i to be separate from Tab, so we can use with ctrl+o
 vim.keymap.set('n', '<C-i>', '<C-i>', { silent = true })
 
 -- Make window only window
