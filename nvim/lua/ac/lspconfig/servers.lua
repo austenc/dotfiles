3167f5f88da7373172fa343ab1a0f2e154b5b909
Austen Cameron <austen.cameron@gmail.com>
2024-10-15 09:07:49 -0600

    wip

---
+++ nvim/lua/ac/lspconfig/servers.lua
@@ -0,0 +1,17 @@
+-------------------------------------------------------------------------------
+-- Configured Lsp Servers
+--------------------------------------------------------------------------------
+
+return {
+    -- intelephense = true,
+    -- -- phpactor = true,
+    -- html = {
+    --     filetypes = { 'html', 'blade', 'antlers' },
+    -- },
+    -- antlersls = true,
+    tailwindcss = true,
+    -- jsonls = true,
+    -- yamlls = true,
+    lua_ls = true,
+    -- vimls = true,
+}
