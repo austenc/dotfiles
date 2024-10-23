e8aef27598b111445ce80a434feab27687022e4b
Austen Cameron <austen.cameron@gmail.com>
2024-10-23 08:12:39 -0600

    wip

---
+++ nvim-old/init.lua
@@ -0,0 +1,30 @@
+-- Keymaps and Config
+require('ac.keymaps')
+require('ac.options')
+
+-- 📦 Lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Lazy.nvim Initialization
+require('lazy').setup('ac.plugins', {
+    ui = {
+        border = 'rounded'
+    },
+    -- automatically check for plugin updates
+    checker = { enabled = true },
+})
+
