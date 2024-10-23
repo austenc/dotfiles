79142bf70c9c8a02e5dbb9af81622808de017045
Austen Cameron <austen.cameron@gmail.com>
2024-10-23 10:23:12 -0600

    wip

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -6,5 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
---
+++ nvim/lua/plugins/nvimtree.lua
@@ -0,0 +1,14 @@
+return {
+  -- {
+  'nvim-tree/nvim-tree.lua',
+  opts = {
+    view = {
+      side = 'right'
+    }
+  },
+  -- cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+  -- opts = function()
+  -- return require "nvchad.configs.nvimtree"
+  -- end,
+  -- },
+}
