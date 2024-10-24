7843f8127f121b152c9b664bd908d33a143bc667
Austen Cameron <austen.cameron@gmail.com>
2024-10-24 10:11:21 -0600

    wip

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,19 +1,16 @@
-require "nvchad.mappings"
-
--- add yours here
+require 'nvchad.mappings'
 
 local map = vim.keymap.set
 
 -- Wish we could have this, but it breaks repeat / next with f and t
 -- map("n", ";", ":", { desc = "CMD enter command mode" })
 
-map("i", "jk", "<ESC>")
+map('i', 'jk', '<ESC>')
 
 -- Toggle file tree on and off
-map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
+map('n', '<leader>e', '<cmd>NvimTreeToggle<CR>', { desc = 'nvimtree toggle window' })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
-
 
 -- Window management
 -- Vertical split
@@ -39,15 +36,16 @@ map('n', '<Leader>=', 'ggVG=', { desc = 'autoindent entire file' })
 map('i', ';;', '<Esc>A;<Esc>')
 map('i', ',,', '<Esc>A,<Esc>')
 
-
 -- Settings mapped to <leader>, combos
-map("n", "<leader>,t", function() require("nvchad.themes").open() end, { desc = "telescope nvchad themes" })
-map("n", "<leader>,k", "<cmd>WhichKey <CR>", { desc = "keyboard shortcuts" })
-map("n", "<leader>,K", function()
-  vim.cmd("WhichKey " .. vim.fn.input "WhichKey: ")
-end, { desc = "search key bindings" })
+map('n', '<leader>,t', function()
+    require('nvchad.themes').open()
+end, { desc = 'telescope nvchad themes' })
+map('n', '<leader>,k', '<cmd>WhichKey <CR>', { desc = 'keyboard shortcuts' })
+map('n', '<leader>,K', function()
+    vim.cmd('WhichKey ' .. vim.fn.input 'WhichKey: ')
+end, { desc = 'search key bindings' })
 
 -- Toggle terminal
-map({ "n", "t" }, "<leader>t", function()
-	require("nvchad.term").toggle { pos = "sp", id = "htoggleTerm" }
+map({ 'n', 't' }, '<leader>t', function()
+    require('nvchad.term').toggle { pos = 'sp', id = 'htoggleTerm' }
 end, { desc = 'toggle terminal' })
