54f2b0bd6658e301c25f88fb29e9a78e0fbcd6cd
Austen Cameron <austen.cameron@gmail.com>
2024-10-28 22:35:53 -0600

    wip

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,11 +1,19 @@
 return {
   'folke/which-key.nvim',
   opts = {
+    preset = 'modern',
     spec = {
       {
         mode = { 'n', 'v' },
+        { '<leader>,', group = 'Settings', icon = { icon = '', color = 'green' } },
         { '<leader>x', group = 'Close File', icon = { icon = '󰱝 ', color = 'green' } },
       },
     },
+    icons = {
+      group = '',
+    },
+  },
+  keys = {
+    { '<leader>?', false },
   },
 }
