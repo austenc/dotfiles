e8ab084bd6d11fb1c5fdc498a2e8356bc43b31c1
Austen Cameron <austen.cameron@gmail.com>
2024-10-29 22:02:36 -0600

    wip

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -5,5 +5,24 @@ return {
     window = {
       position = 'right',
     },
+    close_if_last_window = true,
+    sort_case_insensitive = true,
+    filesystem = {
+      visible = true,
+      follow_current_file = { enabled = true },
+      use_libuv_file_watcher = true,
+      filtered_items = {
+        hide_dotfiles = true,
+        hide_gitignored = true,
+        always_show = {
+          '.prettierrc',
+          '.github',
+        },
+        always_show_by_pattern = {
+          '.env*',
+          'core*',
+        },
+      },
+    },
   },
 }
