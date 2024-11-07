e53ff59d9ae3edaa64a20329f5d9716cb9caaf62
Austen Cameron <austen.cameron@gmail.com>
2024-11-06 21:39:52 -0700

    wip

--- nvim/lua/plugins/testing.lua
+++ nvim/lua/plugins/testing.lua
@@ -1,3 +1,6 @@
+-- Store the terminal instance globally
+local test_term = nil
+
 return {
   'vim-test/vim-test',
   config = function()
@@ -5,27 +8,30 @@ return {
     local function run_in_test_term(cmd)
       -- For debugging the command that is being run
       -- print('Test command:', cmd)
-      local test_term = require('toggleterm.terminal').Terminal:new {
-        -- Remove the --colors flag from the command because `artisan test` passes it in already
-        cmd = cmd:gsub('%-%-colors', ''),
-        direction = 'float',
-        float_opts = {
-          border = 'curved',
-          width = 120,
-          height = 14,
-          row = 14,
-        },
-        close_on_exit = false, -- Keeps the terminal open
-        hidden = true,
-        on_open = function(term)
-          -- Mapping to close the terminal manually
-          vim.cmd 'startinsert!'
-          vim.api.nvim_buf_set_keymap(term.bufnr, 't', '<Esc>', '<cmd>close<CR>', { noremap = true, silent = true })
-          vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
-        end,
-        -- Allow us to navigate the terminal output
-        on_exit = function(term) vim.cmd 'stopinsert' end,
-      }
+      if test_term == nil then
+        test_term = require('toggleterm.terminal').Terminal:new {
+          -- Remove the --colors flag from the command because `artisan test` passes it in already
+          cmd = cmd:gsub('%-%-colors', ''),
+          display_name = 'Test Runner Output',
+          direction = 'float',
+          float_opts = {
+            border = 'curved',
+            width = 120,
+            height = 14,
+            row = 14,
+          },
+          close_on_exit = false, -- Keeps the terminal open
+          hidden = true,
+          on_open = function(term)
+            -- Mapping to close the terminal manually
+            vim.cmd 'startinsert!'
+            vim.api.nvim_buf_set_keymap(term.bufnr, 't', '<Esc>', '<cmd>close<CR>', { noremap = true, silent = true })
+            vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
+          end,
+          -- Allow us to navigate the terminal output
+          on_exit = function(term) vim.cmd 'stopinsert' end,
+        }
+      end
       test_term:toggle()
     end
 
@@ -41,6 +47,13 @@ return {
     { '<Leader>kf', '<cmd>w<CR><cmd>TestFile<CR>', desc = 'Run Tests in File' },
     { '<Leader>kp', '<cmd>w<CR><cmd>TestLast<CR>', desc = 'Run Previous Test' },
     { '<Leader>kr', 'zt<cmd>w<CR><cmd>TestNearest<CR>', desc = 'Run Nearest Test' },
-    -- { '<Leader>kv', ':w<CR>:TestVisit<CR>', desc = 'Run Visit Test??' },
+    {
+      '<Leader>ko',
+      function()
+        if test_term then test_term:toggle() end
+      end,
+      desc = 'View Test Output',
+    },
+    { '<Leader>kv', '<cmd>w<CR><cmd>TestVisit<CR>', desc = 'Visit (return) to last Test' },
   },
 }
