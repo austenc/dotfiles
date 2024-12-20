540cdd84b03beda6390fe239beb423d4676acddf
Austen Cameron <austen.cameron@gmail.com>
2024-12-20 12:28:40 -0700

    wip

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,31 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require 'config.lazy'
+
+-- Configure neovide
+-- if vim.g.neovide then
+--   -- Helper function for transparency formatting
+--   local alpha = function()
+--     local opacity = 255 * vim.g.neovide_transparency
+--     return string.format('%x', math.floor(opacity or 0.1))
+--   end
+--   vim.g.neovide_transparency = 0.1
+--   vim.g.neovide_cursor_antialiasing = true
+--   vim.g.neovide_window_blurred = true
+--   vim.g.neovide_background = '#15191e' .. alpha()
+--
+--   vim.g.neovide_cursor_vfx_mode = 'pixiedust'
+--
+--   -- Command key mappings
+--   vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
+--   vim.keymap.set('v', '<D-c>', '"+y') -- Copy
+--   vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
+--   vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
+--   vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
+--   vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+--
+--   -- Scaling
+--   vim.g.neovide_scale_factor = 1.0
+--   local change_scale_factor = function(delta) vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta end
+--   vim.keymap.set('n', '<C-=>', function() change_scale_factor(1.25) end)
+--   vim.keymap.set('n', '<C-->', function() change_scale_factor(1 / 1.25) end)
+-- end
